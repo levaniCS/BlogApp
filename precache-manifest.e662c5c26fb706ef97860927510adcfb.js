cf281b160ac50e15baf28de168dfb851306e40c7
levaniCS <levanisarishvili3105@gmail.com>
2020-03-14 22:28:28 +0400

    Updates

---
+++ precache-manifest.e662c5c26fb706ef97860927510adcfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ca2faf470c9f7a05cbca689175b86d",
+    "url": "/blogapp/index.html"
+  },
+  {
+    "revision": "aece8758a6da9c0f1c28",
+    "url": "/blogapp/static/css/main.cf29ae00.chunk.css"
+  },
+  {
+    "revision": "2e2762f1c72cf780a0de",
+    "url": "/blogapp/static/js/2.ee3fd17a.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/blogapp/static/js/2.ee3fd17a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aece8758a6da9c0f1c28",
+    "url": "/blogapp/static/js/main.33469285.chunk.js"
+  },
+  {
+    "revision": "96a3ae961411136c75e6",
+    "url": "/blogapp/static/js/runtime-main.83f99a80.js"
+  }
+]);
